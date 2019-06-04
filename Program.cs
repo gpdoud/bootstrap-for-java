f69ec73d6c76e4a483d9806d97db81ed4434a17c
Greg Doud <gpdoud@users.noreply.github.com>
2019-06-04 16:25:50 -0400

    Added some CSharp files

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace JavaSumNbrsOneToTwentyDivisibleByThreeOrFive {
+    class Account { }
+    class Savings : Account { }
+    class Program {
+        static void Main(string[] args) {
+            (new Program()).Run();
+        }
+
+        void Run() {
+
+            var total = 0;
+            // iterate the numbers 1 to 20
+            for(int i = 1; i <=20; i++) {
+
+                // check divisibility by 3 or 5
+                if(i % 3 == 0 || i % 5 == 0) {
+                    total += i;
+                }
+
+            }
+            // display the sum
+            Console.WriteLine($"The total is {total}");
+
+            int[] ints = { 1, 2, 3, 4, 5 };
+            foreach(var i in ints) { }
+        }
+    }
+}
---
+++ Vendor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JavaWebApi {
+    public class Vendor {
+
+        [Key]
+        public int Id { get; set; }
+        [StringLength(30)]
+        [Required]
+        public string Name { get; set; }
+        [StringLength(30)]
+        [Required]
+        public string Address { get; set; }
+
+        [StringLength(20)]
+        public string City { get; set; }
+
+        [StringLength(12)]
+        [Required]
+        public string Phone { get; set; }
+        [StringLength(255)]
+        public string Email { get; set; }
+
+        public Vendor() {
+
+        }
+    }
+}
